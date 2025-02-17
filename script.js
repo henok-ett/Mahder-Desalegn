2e3ac9ebbd042d811d8619c2054228e866961abd
henok-ett <kindehenok@gmail.com>
2025-02-17 10:14:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -49,6 +49,31 @@ addEventOnElements(navTogglers, "click", function () {
   document.body.classList.toggle("nav-active");
 });
 
+*
+ * add event on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * Mobile navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+addEventOnElements(navTogglers, "click", function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
 addEventOnElements(navLinks, "click", function () {
   navbar.classList.remove("active");
   overlay.classList.remove("active");
