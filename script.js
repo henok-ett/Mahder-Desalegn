5fd637e98c7a06097005d81a97355bdd3fcf9aa2
henok-ett <kindehenok@gmail.com>
2025-03-07 03:29:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -73,6 +73,39 @@ window.addEventListener("scroll", function () {
  * Element tilt effect
  */
 
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+addEventOnElements(navTogglers, "click", function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+});
+
+addEventOnElements(navLinks, "click", function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+  document.body.classList.remove("nav-active");
+});
+
+
+
+/**
+ * Header active
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
+});
+
+
+
+/**
+
 const tiltElements = document.querySelectorAll("[data-tilt]");
 
 const initTilt = function (event) {
